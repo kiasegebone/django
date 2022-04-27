29268030993b739a835aa10e30387eff4c01875f
root <root@ip-172-31-10-205.ec2.internal>
2022-04-27 09:19:14 +0000

    Fix Fix Argument Swap
    In file: base.py, method: _get_response_async, there is a call to a method in which the arguments are passed in the wrong order. This may change the outcome of the program. iCR swapped the method arguments to follow the proper order.

--- django/core/handlers/base.py
+++ django/core/handlers/base.py
@@ -235,7 +235,7 @@ class BaseHandler:
                 response = await sync_to_async(
                     self.process_exception_by_middleware,
                     thread_sensitive=True,
-                )(e, request)
+                )(request, e)
                 if response is None:
                     raise
 
